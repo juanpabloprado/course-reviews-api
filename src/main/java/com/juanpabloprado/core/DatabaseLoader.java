626c38e21fa8f189d12dd4956d70f58009e72ad7
juanpabloprado <juan@juanpabloprado.com>
2016-10-12 20:55:22 -0500

    Add gitignore

--- src/main/java/com/juanpabloprado/core/DatabaseLoader.java
+++ src/main/java/com/juanpabloprado/core/DatabaseLoader.java
@@ -2,6 +2,7 @@ package com.juanpabloprado.core;
 
 import com.juanpabloprado.course.Course;
 import com.juanpabloprado.course.CourseRepository;
+import com.juanpabloprado.review.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -18,6 +19,14 @@ public class DatabaseLoader implements ApplicationRunner {
 
   @Override public void run(ApplicationArguments args) throws Exception {
     Course course = new Course("Java Basics", "https://teamtreehouse.com/library/java-basics");
+    course.addReview(new Review(3, "You ARE a dork!!!"));
+    String[] templates = {
+        "Up and running with %s",
+        "%s Basics",
+        "%s for Beginners",
+        "%s for Neckbeards",
+        "Under the hood: %s"
+    };
     courses.save(course);
   }
 }
