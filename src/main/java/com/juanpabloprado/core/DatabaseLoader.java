707d84430b5f2f30147d6f2502614be304ce9cd7
juanpabloprado <juan@juanpabloprado.com>
2016-10-13 10:39:34 -0500

    Add HAL browser

--- src/main/java/com/juanpabloprado/core/DatabaseLoader.java
+++ src/main/java/com/juanpabloprado/core/DatabaseLoader.java
@@ -3,6 +3,9 @@ package com.juanpabloprado.core;
 import com.juanpabloprado.course.Course;
 import com.juanpabloprado.course.CourseRepository;
 import com.juanpabloprado.review.Review;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -20,6 +23,8 @@ public class DatabaseLoader implements ApplicationRunner {
   @Override public void run(ApplicationArguments args) throws Exception {
     Course course = new Course("Java Basics", "https://teamtreehouse.com/library/java-basics");
     course.addReview(new Review(3, "You ARE a dork!!!"));
+    courses.save(course);
+
     String[] templates = {
         "Up and running with %s",
         "%s Basics",
@@ -27,6 +32,27 @@ public class DatabaseLoader implements ApplicationRunner {
         "%s for Neckbeards",
         "Under the hood: %s"
     };
-    courses.save(course);
+    String[] buzzwords = {
+        "Spring REST Data",
+        "Java 9",
+        "Scala",
+        "Groovy",
+        "Hibernate",
+        "Spring HATEOAS"
+    };
+
+    List<Course> bunchOfCourses = new ArrayList<>();
+    IntStream.range(0, 100)
+        .forEach(i -> {
+          String template = templates[i % templates.length];
+          String buzzword = buzzwords[i % buzzwords.length];
+          String title = String.format(template, buzzword);
+          Course c = new Course(title, "http://example.com");
+          c.addReview(new Review(i % 5, String.format("Moar %s please!!!!", buzzword)));
+          bunchOfCourses.add(c);
+        });
+
+    courses.save(bunchOfCourses);
+
   }
 }
